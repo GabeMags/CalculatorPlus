3363ffb790b86948f583460e7159bb090c1f306e
GabeMags <magallanes.gabe@gmail.com>
2020-09-09 14:21:09 -0700

    Finished the splashscreen and it's progress bar.

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
+# This app was created to be a calculator with fun and unexpected features. It is a 3-manned project overall.
+# This is Gabriel Magallanes's contributions (for better or for worse)
+# CSUF Fall 2020 - CPSC
+
 import random
 import tkinter as tk
 import time
 # Note: I had to manually install imagetk with the command:  sudo apt-get install python3-pil.imagetk
+from tkinter.ttk import *
+from tkinter import *
 from PIL import Image, ImageTk
 
-# Various settings. Maybe make a settings file?
-window_width = 480
+
+# Various settings that the whole app will use. Maybe I'll make a settings file?
+window_width = 1280
 window_height = 720
 app_title = "Calculator Plus"
 
@@ -17,20 +24,68 @@ class SplashScreen(tk.Toplevel):
         self.title(app_title)
         self.geometry("%dx%d+0+0" % (window_width, window_height))
 
+        # Create a frame to pack the widgets in
+        canvas = Frame(self)
+        canvas.pack(fill=BOTH, expand=True, padx=5, pady=5)
+
         # Set the splash background image. ImageTk needs to be used for expanded image file compatibility
         input_image = Image.open("splash.jpg")
         background = ImageTk.PhotoImage(input_image)
 
+        # Abandoned style code to make the progress bar have an image instead of a color
+        # This code block goes before the splash_progress_bar is initialized
+        #bar_style = Style(self)  # Style() uses ttk
+        # add a label in the layout
+        #bar_style.layout('Horizontal.TProgressbar',
+        #             [('Horizontal.Progressbar.trough',
+        #               {'children': [('Horizontal.Progressbar.splash_progress_bar',
+        #                              {'side': 'left', 'sticky': 'ns'})],
+        #                'sticky': 'nswe'}),
+        #              ('Horizontal.Progressbar.label', {'sticky': ''})])
+        #
+        # set the label to have a background image that expands with the progress bar
+        #bar_style.configure('Horizontal.TProgressbar.label', image=background, fill=X, expand=True)
+
+        # Fake progress bar widget
+        # (determinate mode animates it for us)
+        splash_progress_bar = Progressbar(canvas, orient=HORIZONTAL, length=100, mode='determinate')
+        splash_progress_bar.pack(side=TOP, fill=X, padx=5, pady=5)
+
         # Pack the background with the splash text on top
-        panel = tk.Label(self, image=background, text=app_title, font=("Arial", 25, "bold"), compound="center")
-        panel.pack(fill='both', expand='yes')
+        panel = tk.Label(canvas, image=background, text=app_title, font=("Arial", 25, "bold"), compound="center")
+        panel.pack(side=TOP, fill=X)
         panel.image = background
 
-        # Implement the loading bar (extra- not implemented yet)
-        # Nothing here!
+        # Function responsible for
+        # the progress bar value.
+        # The splash destructs after this bar is done
+        def fake_loading():
+            import time
+            splash_progress_bar['value'] = 20
+            self.update_idletasks()
+            time.sleep(0.5)
 
-        # This makes the window show before the mainloop
-        self.update()
+            splash_progress_bar['value'] = 40
+            self.update_idletasks()
+            time.sleep(0.5)
+
+            splash_progress_bar['value'] = 50
+            self.update_idletasks()
+            time.sleep(0.5)
+
+            splash_progress_bar['value'] = 60
+            self.update_idletasks()
+            time.sleep(0.5)
+
+            splash_progress_bar['value'] = 80
+            self.update_idletasks()
+            time.sleep(0.5)
+
+            splash_progress_bar['value'] = 100
+            self.update_idletasks()
+            time.sleep(0.5)
+
+        fake_loading()
 
 
 class App(tk.Tk):
@@ -45,16 +100,15 @@ class App(tk.Tk):
 
         # ----LOADING SCREEN-----
         # Simulate a loading delay in seconds
-        time.sleep(3)
+        # (this was removed since the loading bar now determines load duration)
+        #time.sleep(2)
+
         # Finished loading so destroy splash
         splash.destroy()
         # Show window again
         self.deiconify()
 
         # -----MAIN APP-----
-
-
-
 
 
 if __name__ == "__main__":
