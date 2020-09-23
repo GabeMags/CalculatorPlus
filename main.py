1d7939b60f5db31e9e95b0a76a83e83b3247b1e8
GabeMags <magallanes.gabe@gmail.com>
2020-09-23 00:23:28 -0700

    I fused in some functions I was experimenting with so that the splash screen is using tk for its layout, but the main application uses pygame for the UI logic.
    I plan to redesign the home screen. It's black and red for testing purposes.
    
    Each button takes you to a different page for the calculator

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # This is Gabriel Magallanes's contributions (for better or for worse)
 # CSUF Fall 2020 - CPSC
 
+import pygame, sys
+import os  # I'm using this to make sure that my external code is included
 import random
 import tkinter as tk
 import time
@@ -9,11 +11,22 @@ import time
 from tkinter.ttk import *
 from tkinter import *
 from PIL import Image, ImageTk
+from pygame.locals import *
 
-# Various settings that the whole app will use. Maybe I'll make a settings file?
+mainClock = pygame.time.Clock()
+
+pygame.init()
+
+# Application settings
+app_title = 'Calculator Plus'
 window_width = 1280
 window_height = 720
-app_title = "Calculator Plus"
+# Just to make things easy
+color_white = (255, 255, 255)
+color_red = (255, 0, 0)
+color_black = (0, 0, 0)
+center_coordinates = (640, 360)  # (x, y)
+center_top_coordinates = '640, 20'
 
 
 class SplashScreen(tk.Toplevel):
@@ -30,20 +43,6 @@ class SplashScreen(tk.Toplevel):
         # Set the splash background image. ImageTk needs to be used for expanded image file compatibility
         input_image = Image.open("splash.jpg")
         background = ImageTk.PhotoImage(input_image)
-
-        # Abandoned style code to make the progress bar have an image instead of a color
-        # This code block goes before the splash_progress_bar is initialized
-        # bar_style = Style(self)  # Style() uses ttk
-        # add a label in the layout
-        # bar_style.layout('Horizontal.TProgressbar',
-        #             [('Horizontal.Progressbar.trough',
-        #               {'children': [('Horizontal.Progressbar.splash_progress_bar',
-        #                              {'side': 'left', 'sticky': 'ns'})],
-        #                'sticky': 'nswe'}),
-        #              ('Horizontal.Progressbar.label', {'sticky': ''})])
-        #
-        # set the label to have a background image that expands with the progress bar
-        # bar_style.configure('Horizontal.TProgressbar.label', image=background, fill=X, expand=True)
 
         # Fake progress bar widget
         # (determinate mode animates it for us)
@@ -79,17 +78,146 @@ class App(tk.Tk):
         self.title(app_title)
         self.geometry("%dx%d+0+0" % (window_width, window_height))
 
-        # ----LOADING SCREEN-----
-        # Simulate a loading delay in seconds
-        # (this was removed since the loading bar now determines load duration)
-        # time.sleep(2)
-
         # Finished loading so destroy splash
         splash.destroy()
         # Show window again
-        self.deiconify()
+        # self.deiconify()
 
-        # -----MAIN APP-----
+        # -----MAIN APP----- This is where pygame comes into play for main GUI
+        # pygame.display.set_caption(app_title)
+        self.screen = pygame.display.set_mode((window_width, window_height), 0, 32)
+
+        screen = self.screen
+
+        app_font_and_size = pygame.font.SysFont(None, 20)  # Size 20 font with default font type
+
+        # Function to draw text wherever I need to
+        # centered is a boolean- if false, then use x y
+        # is_title_text is a boolean- if false, then use x y
+        def draw_text(text, font, font_color, surface, x, y, true_centered, is_title_text):
+            text_obj = font.render(text, 1, font_color)
+
+            if not true_centered:
+                if not is_title_text:
+                    text_rect = text_obj.get_rect()  # Todo: get rect son lol
+                    text_rect.top_left = (x, y)  # set the text rectangle to be the literal x,y passed to this func
+                if is_title_text:
+                    text_rect = text_obj.get_rect(center=(window_width / 2, 20))  # Special spot for the title only
+
+            if true_centered:  # Todo: who knows if I'll actually use this
+                text_rect = text_obj.get_rect(center=(window_width / 2, window_height / 2))  # Todo: Lol get rect son
+
+            surface.blit(text_obj, text_rect)
+
+        click = False
+
+        def home_screen():
+            while True:
+                screen.fill((0, 0, 0))
+                # Todo: I want to change this from just text to a graphic for the title? maybe?
+                # Technically the x and y don't matter here so I made them 404 (arbitrary)
+                draw_text(app_title, app_font_and_size, color_white, screen, 404, 404, False, True)
+
+                # Mouse cursor location tracking
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+
+                # Button objects [without text] and their locations
+                button_kids_calc = pygame.Rect(280, 500, 200, 50)  # Rect(dist from left, dist from top, width, height)
+                button_std_calc = pygame.Rect(560, 500, 200, 50)
+                button_camera_calc = pygame.Rect(840, 500, 200, 50)
+                button_options = pygame.Rect(1160, 500, 200, 50)
+
+                # Logic for clicking on the buttons (maybe make this into a switch case? i know its not elegant)
+                # Yes, buttons are basically collision-tracking rectangles.
+                if button_kids_calc.collidepoint((mouse_x, mouse_y)):
+                    if click:
+                        kids_calculator()
+                if button_std_calc.collidepoint((mouse_x, mouse_y)):
+                    if click:
+                        standard_calculator()
+                if button_camera_calc.collidepoint((mouse_x, mouse_y)):
+                    if click:
+                        pass  # Todo: camera
+                if button_options.collidepoint((mouse_x, mouse_y)):
+                    if click:
+                        pass  # Todo: options
+
+                # Drawing buttons to the screen with their styles
+                pygame.draw.rect(screen, color_red, button_kids_calc)
+                pygame.draw.rect(screen, color_red, button_std_calc)
+                pygame.draw.rect(screen, color_red, button_camera_calc)
+                pygame.draw.rect(screen, color_red, button_options)
+
+                click = False
+
+                # Check for keypress/clicks
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        pygame.quit()
+                        sys.exit()
+                    if event.type == KEYDOWN:
+                        if event.key == K_ESCAPE:
+                            pygame.quit()
+                            sys.exit()
+                    if event.type == MOUSEBUTTONDOWN:
+                        if event.button == 1:
+                            click = True
+
+                pygame.display.update()
+                mainClock.tick(60)  # Todo: Idk what this really does
+
+        def kids_calculator():
+            running = True
+            while running:
+                screen.fill(color_black)
+
+                draw_text('Kids Calculator', app_font_and_size, color_white, screen, 20, 20, False, True)
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        pygame.quit()
+                        sys.exit()
+                    if event.type == KEYDOWN:
+                        if event.key == K_ESCAPE:
+                            running = False
+
+                pygame.display.update()
+                mainClock.tick(60)  # Todo: Idk what this really does
+
+        def standard_calculator():
+            running = True
+            while running:
+                screen.fill(color_black)
+
+                draw_text('Standard Calculator', app_font_and_size, color_white, screen, 20, 20, False, True)
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        pygame.quit()
+                        sys.exit()
+                    if event.type == KEYDOWN:
+                        if event.key == K_ESCAPE:
+                            running = False
+
+                pygame.display.update()
+                mainClock.tick(60)  # Todo: Idk what this really does
+
+        def camera_calculator():
+            running = True
+            while running:
+                screen.fill(color_black)
+
+                draw_text('Camera Calculator', app_font_and_size, color_white, screen, 20, 20)
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        pygame.quit()
+                        sys.exit()
+                    if event.type == KEYDOWN:
+                        if event.key == K_ESCAPE:
+                            running = False
+
+                pygame.display.update()
+                mainClock.tick(60)  # Todo: Idk what this really does
+
+        home_screen()
 
 
 if __name__ == "__main__":
