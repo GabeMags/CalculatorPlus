a477a623f0305c5fbf38b8c896bd12567365a519
Sean Mitchell <toomanyseans@gmail.com>
2020-10-10 12:03:54 -0700

    Created a math library file to use with GUI.

---
+++ MathLib.py
@@ -0,0 +1,23 @@
+import random
+
+
+# Generates a random math question with numbers including 0-9 and operators plus or minus. Returns a string.
+def gen_question():
+    operator = "+-"
+    numbers = "0123456789"
+    equation = random.choice(numbers) + " " + random.choice(operator) + " " + random.choice(numbers)
+    return equation
+
+
+# Evaluates a math question. Returns the answer in a string.
+def evaluate_question(equation):
+    try:
+        answer = eval(equation)
+    except:
+        answer = "ERROR"
+    return answer
+
+
+# Compares the user the inputted answer with the actual answer, returns a boolean.
+def compare_answers(actual_answer, user_input_answer):
+    return actual_answer == user_input_answer
