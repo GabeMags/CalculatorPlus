3c6b886bd6ee8e483e9a068db194c2ba78228739
Sean Mitchell <toomanyseans@gmail.com>
2020-10-17 10:19:38 -0700

    Added an optional difficulty argument to the gen_question function

--- MathLib.py
+++ MathLib.py
@@ -2,10 +2,20 @@ import random
 
 
 # Generates a random math question with numbers including 0-9 and operators plus or minus. Returns a string.
-def gen_question():
+def gen_question(difficulty: int = 1):
+    if difficulty < 1:
+        print("Invalid argument, exiting function.")
+        return
+    difficulty = (difficulty * 2) + 1
     operator = "+-"
     numbers = "0123456789"
-    return random.choice(numbers) + " " + random.choice(operator) + " " + random.choice(numbers)
+    question_list = []
+    for x in range(difficulty):
+        if x % 2 == 0:
+            question_list.insert(x, random.choice(numbers))
+        else:
+            question_list.insert(x, random.choice(operator))
+    return ''.join(question_list)
 
 
 # Evaluates a math question. Returns the answer in a string.
@@ -20,3 +30,6 @@ def evaluate_question(equation):
 # Compares the user the inputted answer with the actual answer, returns a boolean.
 def compare_answers(actual_answer, user_input_answer):
     return str(actual_answer) == str(user_input_answer)
+
+
+print(gen_question(3))
